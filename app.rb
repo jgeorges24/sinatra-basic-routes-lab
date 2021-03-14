7f6b74a2005bce392be7137bf15fa40db3d5084f
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-03-13 20:57:03 -0500

    commit tree c948e78c2c0c43830442d5c1e91c3ed5c3b808e0

--- app.rb
+++ app.rb
@@ -1,4 +1,29 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        "Hello, World!"
+
+    end
+
+
+
+    get '/name' do 
+        "My name is zc the GOD!!!"
+
+    end
+
+    get '/hometown' do 
+        "My hometown is 305!!!"
+
+    end
+
+
+
+    get '/favorite-song' do 
+        "My favorite song is the beats"
+    end
+
+
 end
